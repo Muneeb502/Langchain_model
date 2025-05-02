2c83954893c6fedad94071e21aa7751dae38c419
Muneeb502 <saifurrehman199502@gmail.com>
2025-05-02 18:25:57 +0500

    added files

---
+++ chat_models/chatmodel_hugging_face.py
@@ -0,0 +1,17 @@
+from langchain_huggingface import HuggingFaceEndpoint , ChatHuggingFace
+import os
+from dotenv  import load_dotenv
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(  
+repo_id="microsoft/Phi-3-mini-4k-instruct",  
+task="text-generation",  
+  
+)  
+chat = ChatHuggingFace(llm=llm)
+
+result = chat.invoke("what is the capital of  india ")
+
+print(result.content)
+
---
+++ embedding_models/embed_model_hf_api.py
@@ -0,0 +1,3 @@
+from langchain_huggingface import HuggingFaceEmbeddings , HuggingFaceEndpointEmbeddings
+from dotenv import load_dotenv
+load_dotenv()
